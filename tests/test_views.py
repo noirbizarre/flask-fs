6382401a128ea76003017cb878491ae3a8e4ba7e
Axel Haustant <noirbizarre@gmail.com>
2017-02-27 17:29:52 +0100

    Migrated test_views

--- tests/test_views.py
+++ tests/test_views.py
@@ -1,13 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 
+import pytest
 import mock
-import shutil
-import tempfile
 
 from flask import url_for
-
-from . import TestCase
 
 import flask_fs as fs
 
@@ -19,66 +16,58 @@ class MockBackend(fs.BaseBackend):
 MOCK_BACKEND = '.'.join((__name__, MockBackend.__name__))
 
 
-def mock_backend(func):
-    return mock.patch(MOCK_BACKEND)(func)
+@pytest.fixture
+def app(app):
+    app.config['FS_BACKEND'] = MOCK_BACKEND
+    yield app
 
 
-class ViewsTestCase(TestCase):
-    def setUp(self):
-        super(ViewsTestCase, self).setUp()
-        self.test_dir = tempfile.mkdtemp()
+@pytest.fixture
+def mock_backend():
+    patcher = mock.patch(MOCK_BACKEND)
+    yield patcher.start()
 
-    def tearDown(self):
-        # Remove the directory after the test
-        shutil.rmtree(self.test_dir)
 
-    def configure(self, *storages, **configs):
-        self.app.config['FS_BACKEND'] = configs.pop('FS_BACKEND', MOCK_BACKEND)
-        super(ViewsTestCase, self).configure(*storages, **configs)
+def test_url(app):
+    storage = fs.Storage('test')
 
-    def test_url(self):
-        storage = fs.Storage('test')
+    app.configure(storage)
 
-        self.configure(storage)
+    expected_url = url_for('fs.get_file', fs=storage.name, filename='test.txt')
+    assert storage.url('test.txt') == expected_url
 
-        with self.app.test_request_context('/'):
-            expected_url = url_for('fs.get_file', fs=storage.name, filename='test.txt')
-            self.assertEqual(storage.url('test.txt'), expected_url)
 
-    @mock_backend
-    def test_get_file(self, mock_backend):
-        storage = fs.Storage('test')
-        backend = mock_backend.return_value
-        backend.serve.return_value = 'content'.encode('utf-8')
+def test_get_file(app, mock_backend):
+    storage = fs.Storage('test')
+    backend = mock_backend.return_value
+    backend.serve.return_value = 'content'.encode('utf-8')
 
-        self.configure(storage)
+    app.configure(storage)
 
-        with self.app.test_request_context():
-            file_url = url_for('fs.get_file', fs='test', filename='test.txt')
+    file_url = url_for('fs.get_file', fs='test', filename='test.txt')
 
-        response = self.client.get(file_url)
-        self.assertEqual(response.status_code, 200)
-        self.assertEqual(response.data, 'content'.encode('utf-8'))
+    response = app.test_client().get(file_url)
+    assert response.status_code ==  200
+    assert response.data == 'content'.encode('utf-8')
 
-    @mock_backend
-    def test_get_file_not_found(self, mock_backend):
-        storage = fs.Storage('test')
-        backend = mock_backend.return_value
-        backend.exists.return_value = False
 
-        self.configure(storage)
+def test_get_file_not_found(app, mock_backend):
+    storage = fs.Storage('test')
+    backend = mock_backend.return_value
+    backend.exists.return_value = False
 
-        with self.app.test_request_context('/'):
-            file_url = url_for('fs.get_file', fs='test', filename='test.txt')
+    app.configure(storage)
 
-        response = self.client.get(file_url)
-        self.assertEqual(response.status_code, 404)
+    file_url = url_for('fs.get_file', fs='test', filename='test.txt')
 
-    def test_get_file_no_storage(self):
-        self.configure()
+    response = app.test_client().get(file_url)
+    assert response.status_code == 404
 
-        with self.app.test_request_context('/'):
-            file_url = url_for('fs.get_file', fs='fake', filename='test.txt')
 
-        response = self.client.get(file_url)
-        self.assertEqual(response.status_code, 404)
+def test_get_file_no_storage(app):
+    app.configure()
+
+    file_url = url_for('fs.get_file', fs='fake', filename='test.txt')
+
+    response = app.test_client().get(file_url)
+    assert response.status_code == 404
